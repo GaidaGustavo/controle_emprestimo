16258ee82a75197cc087e1d15961cf792527355d
Mauro Pellizzaro <mauropellizzaro123@gmail.com>
2024-09-19 20:59:55 -0300

    Criação item controler

---
+++ src/application/controller/item-controller.ts
@@ -0,0 +1,23 @@
+import { ItemRepository } from "../../domain/repository/item-repository";
+import { CreateItemUseCase } from "../use-cases/create-item/create-item";
+import { GetAllItensUseCase } from "../use-cases/get-all-itens/get-all-itens";
+import { UpdateItemUseCase } from "../use-cases/update-item/update-item";
+
+export class ItemController{
+
+    constructor(private readonly itemRepository: ItemRepository){}
+
+    create(input: any){
+        const createItemUseCase = new CreateItemUseCase(this.itemRepository)
+        createItemUseCase.execute(input);
+    }
+    update(input: any) {
+        const updateItemUseCase = new UpdateItemUseCase(this.itemRepository)
+        updateItemUseCase.execute(input);
+    }
+    getAll(input: any){
+        const getAllItensUseCase = new GetAllItensUseCase(this.itemRepository)
+        getAllItensUseCase.execute(input);
+
+    }
+}
