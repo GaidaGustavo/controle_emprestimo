d9d13a07d0ffff99d6be1044fdcda2dda02057b5
Mauro Pellizzaro <mauropellizzaro123@gmail.com>
2024-09-19 21:10:49 -0300

    itemcontrol

--- src/application/controller/item-controller.ts
+++ src/application/controller/item-controller.ts
@@ -1,13 +1,14 @@
 import { ItemRepository } from "../../domain/repository/item-repository";
 import { CreateItemUseCase } from "../use-cases/create-item/create-item";
 import { GetAllItensUseCase } from "../use-cases/get-all-itens/get-all-itens";
+import { GetItemByIdUseCase } from "../use-cases/get-item-by-id/get-item-by-id";
 import { UpdateItemUseCase } from "../use-cases/update-item/update-item";
 
 export class ItemController{
 
     constructor(private readonly itemRepository: ItemRepository){}
 
-    create(input: any){
+    create(input: any) {
         const createItemUseCase = new CreateItemUseCase(this.itemRepository)
         createItemUseCase.execute(input);
     }
@@ -18,6 +19,10 @@ export class ItemController{
     getAll(input: any){
         const getAllItensUseCase = new GetAllItensUseCase(this.itemRepository)
         getAllItensUseCase.execute(input);
-
     }
+    getItemById(input: any) {
+        const getItemByIdUseCase = new GetItemByIdUseCase(this.itemRepository)
+        getItemByIdUseCase.execute(input);  
+    }
+  
 }
