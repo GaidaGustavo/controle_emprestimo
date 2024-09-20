cd044f2030eaaea553566bbee641c70a1fc994b0
lucasemic <lucasmichaelsen6@gmail.com>
2024-09-19 21:23:47 -0300

    Commit

--- src/application/use-cases/get-tipo-item-by-id/get-tipo-item-by-id.ts
+++ src/application/use-cases/get-tipo-item-by-id/get-tipo-item-by-id.ts
@@ -1,10 +1,11 @@
 import { TipoItem } from "../../../domain/entity/tipoitem";
+import { TipoItemRepository } from "../../../domain/repository/tipoitem-repository";
 import { GetTipoItemByIdInput } from "./get-tipo-item-by-id-input";
 import { GetTipoItemByIdOutput } from "./get-tipo-item-by-id-output";
 
 
 export class GetTipoitemByIdUseCase {
-    constructor(readonly tipoItem: TipoItem) {}
+    constructor(readonly tipoItem: TipoItemRepository) {}
     
     execute(input: GetTipoItemByIdInput):GetTipoItemByIdOutput {
         return {} as GetTipoItemByIdOutput;
