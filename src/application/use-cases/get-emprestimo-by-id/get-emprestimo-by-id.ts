26d686e12c32b0d92e40155bf27b03c204c46d96
GaidaGustavo <gustavo.gaida@gmail.com>
2024-09-24 19:52:51 -0300

    Criação da primeira regra de um GetById

--- src/application/use-cases/get-emprestimo-by-id/get-emprestimo-by-id.ts
+++ src/application/use-cases/get-emprestimo-by-id/get-emprestimo-by-id.ts
@@ -1,12 +1,34 @@
 import { EmprestimoRepository } from "../../../domain/repository/emprestimo-repository";
+import { GetUsuarioByIdInput } from "../get-usuario-by-id/get-usuario-by-id-input";
 import { GetEmprestimoByIdInput } from "./get-emprestimo-by-id-input";
 import { GetEmprestimoByIdOutput } from "./get-emprestimo-by-id-output";
 
 
 export class GetEmprestimoByIdUseCase {
-    constructor(readonly emprestimoRepository: EmprestimoRepository) {}
-    
-    execute(input: GetEmprestimoByIdInput):GetEmprestimoByIdOutput {
-        return {} as GetEmprestimoByIdOutput;
+    constructor(private readonly emprestimoRepository: EmprestimoRepository) { }
+
+    execute(input: GetEmprestimoByIdInput): GetEmprestimoByIdOutput {
+        const emprestimo = this.emprestimoRepository.getById(input.id);
+
+        const output: GetEmprestimoByIdOutput = {
+            id: emprestimo.getID(),
+            item: {
+                id: emprestimo.getItem().getID(),
+                nome: emprestimo.getItem().getName(),
+                tipoItem: {
+                    id: emprestimo.getItem().getItem().getID(),
+                    nome: emprestimo.getItem().getItem().getName(),
+                },
+            },
+            dataEmprestimo: emprestimo.getdataEmprestimo(),
+            dataDevolucao: emprestimo.getdataDevolucao(),
+            pessoa: {
+                id: emprestimo.getPessoa().getID(),
+                nome: emprestimo.getPessoa().getName(),
+            }
+        }
+
+
+        return output;
     }
 }
