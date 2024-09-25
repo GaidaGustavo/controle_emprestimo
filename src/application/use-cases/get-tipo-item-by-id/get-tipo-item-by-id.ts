78ab8e906b14a989c346f3c9ff4439c771a28d51
GaidaGustavo <gustavo.gaida@gmail.com>
2024-09-24 21:48:26 -0300

    Correction

--- src/application/use-cases/get-tipo-item-by-id/get-tipo-item-by-id.ts
+++ src/application/use-cases/get-tipo-item-by-id/get-tipo-item-by-id.ts
@@ -2,7 +2,6 @@ import { TipoItemRepository } from "../../../domain/repository/tipoitem-reposito
 import { GetTipoItemByIdInput } from "./get-tipo-item-by-id-input";
 import { GetTipoItemByIdOutput } from "./get-tipo-item-by-id-output";
 
-
 export class GetTipoitemByIdUseCase {
     constructor(readonly tipoItemRepository: TipoItemRepository) {}
     
