1dfb61f141614d5ec28719424c144e209b2c5ee2
MauroPellizzaro <mauropellizzaro123@gmail.com>
2024-11-30 16:26:29 -0300

    Update pessoa.ts

--- src/domain/entity/pessoa.ts
+++ src/domain/entity/pessoa.ts
@@ -1,26 +1,29 @@
 import { v4 } from 'uuid';
 
 export class Pessoa {
-    readonly id?: string;
+    private id: string;
+    private name: string;
+    private documento: string;
     
-    constructor(readonly name: string, readonly documento:string, id?: string) {
+    constructor(name: string, documento: string, id?: string) {
         if (!id) {
             id = v4();
-        };
+        }
         this.id = id;
         this.name = name;
         this.documento = documento;
     }
 
-    getID(): string | undefined{
+    // Métodos Getter
+    getID(): string {
         return this.id;
     }
 
-    getName(): string{
+    getName(): string {
         return this.name;
-    }   
+    }
 
-    getDocumento(): string{
+    getDocumento(): string {
         return this.documento;
-    }   
+    }
 }
