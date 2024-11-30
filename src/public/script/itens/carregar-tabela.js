d49057f740fb6da87744b920227bfe0eb8b73ab5
MauroPellizzaro <mauropellizzaro123@gmail.com>
2024-11-30 20:59:03 -0300

    Update carregar-tabela.js

--- src/public/script/itens/carregar-tabela.js
+++ src/public/script/itens/carregar-tabela.js
@@ -42,7 +42,7 @@ function carregarItens() {
                     <td>${dataFormatada}</td>
                     <td>
                         <button class="btn btn-success orientacao" onclick="modalEdicao('${item.id}', '${item.name}', '${item.tipoItem.id}', '${item.itemEPI.ca}' , '${item.itemEPI.validade}')">Editar</button>
-                        <button class="btn btn-danger orientacao" data-bs-toggle="modal"
+                        <button class="btn btn-danger orientacao" disabled data-bs-toggle="modal"
                             data-bs-target="#confirmDeleteModal" onclick="modalExclusaoItem('${item.id}')">Excluir</button>
                     </td>
                 `;
